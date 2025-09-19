a104737e1a66a4d351a3c5e100a3f4a7c396996c
AsherSSB <gromanenghi04@gmail.com>
2025-09-19 12:02:18 -0700

    adds steam review lib file

---
+++ app/lib/steamReviewFetcher.js
@@ -0,0 +1,7 @@
+function main() {
+    const appid = process.argv[2];
+    const reviews = [];
+    // populate reviews (fetch reviews by id)
+    // sort reviews by funny
+    // console log funnies review
+}
